a23aa2269b4baaaf286773ca0872bcaf64700f0d
Fabrizio <fabrisabella25@gmail.com>
2022-06-27 12:58:29 -0300

    agregando documentos

---
+++ portfolio/models.py
@@ -0,0 +1,15 @@
+from distutils.command import upload
+from tokenize import blank_re
+from django.db import models
+from django.forms import CharField, URLField
+from django.db.models.fields.files import ImageField
+
+
+# Create your models here.
+
+
+class Project(models.Model):
+    titulo = CharField(max_length=100)
+    descripcion = CharField(max_length=250)
+    imagen = ImageField(upload_to = "portfolio/imagenes/")
+    url = URLField(max_length=300)
