c4cdaa029207fc95e00d7502a3575dd952320de2
Fabrizio <fabrisabella25@gmail.com>
2022-06-28 17:25:55 -0300

    models.py

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,7 +1,7 @@
 from distutils.command import upload
 from tokenize import blank_re
 from django.db import models
-from django.forms import CharField, URLField
+from django.db.models.fields import CharField, URLField
 from django.db.models.fields.files import ImageField
 
 
@@ -12,4 +12,5 @@ class Project(models.Model):
     titulo = CharField(max_length=100)
     descripcion = CharField(max_length=250)
     imagen = ImageField(upload_to = "portfolio/imagenes/")
-    url = URLField(max_length=300)
+    url = URLField(blank=True)
+
